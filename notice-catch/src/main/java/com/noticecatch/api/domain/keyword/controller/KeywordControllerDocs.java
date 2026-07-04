c5c2a04d040e5ab753127a255b32eda43399a434
Uzrt <worldsilvertree@gmail.com>
2026-07-04 22:18:41 +0900

    controller Docs 수정

--- notice-catch/src/main/java/com/noticecatch/api/domain/keyword/controller/KeywordControllerDocs.java
+++ notice-catch/src/main/java/com/noticecatch/api/domain/keyword/controller/KeywordControllerDocs.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Map;
 
-@Tag(name = "🏷️ Keyword", description = "시스템 마스터 키워드 제공 API")
+@Tag(name = "🏷️ Keyword", description = "추천 키워드 제공 API")
 public interface KeywordControllerDocs {
     @Operation(summary = "추천 키워드 목록 조회", description = "시스템이 기본 제공하는 추천 키워드 리스트를 가져옵니다.")
     ApiResponse<Map<String, Object>> getRecommendKeywords();
