36db08ce83ef1fc289bf054a3157ac468fa49ece
Uzrt <worldsilvertree@gmail.com>
2026-07-22 22:22:44 +0900

    feat : test 코드 추가

--- src/main/java/com/noticecatch/api/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/com/noticecatch/api/domain/auth/dto/request/LoginRequest.java
@@ -1,10 +1,12 @@
 package com.noticecatch.api.domain.auth.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     private String socialToken; // 소셜 Access Token
     private String socialType;  // "KAKAO" 또는 "GOOGLE"
